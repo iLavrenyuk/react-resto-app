23a8059520a0d5b5c6ae06eba529cbc2c98a19ac
Илья Лавренюк <48129985+iLavrenyuk@users.noreply.github.com>
2019-07-01 20:31:48 +0300

    Lesson 15

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { MainPage, CartPage } from '../pages';
 import AppHeader from '../app-header';
-import WithRestoService from '../hoc';
 import { Route, Switch } from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
-const App = ({ RestoService }) => {
-    console.log(RestoService.getMenuItems());
+const App = () => {
     return (
         <div style={{ background: `url(${Background}) center center/cover no-repeat` }} className="app">
             <AppHeader total={50} />
@@ -24,4 +22,4 @@ const App = ({ RestoService }) => {
     )
 }
 
-export default WithRestoService()(App);
+export default App;
